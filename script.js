856365b6a5251c35106c45174590d6fbd7777fb8
mmatyjasek <matyjasek.marcin@gmail.com>
2018-07-24 17:41:35 +0200

    api root correction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://marcin-matyjasek.herokuapp.com/v1/task/';
+  const apiRoot = 'https://marcin-matyjasek.herokuapp.com/v1/';
   const trelloApiRoot = 'https://marcin-matyjasek.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
